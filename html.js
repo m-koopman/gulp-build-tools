e9f03c902c837007d64ef5a1eb46d3e4fa8483e7
Max Koopman <m_koopman@me.com>
2015-06-12 14:33:17 +1000

    Added HTML module with jade support

---
+++ html.js
@@ -0,0 +1,27 @@
+var build = require('./build.js');
+
+var gulp = require('gulp'),
+    gutil = require('gulp-util');
+
+var jade = require('gulp-jade');
+
+var HTML = {};
+
+HTML.jade = function(src_globs, dest_folder, label) {
+    label = label || build.globsToString(src_globs);
+
+    jadeComplete = function() {
+        build.log( " jade ",
+            gutil.colors.cyan( label ),
+            "compiled, copied to",
+            gutil.colors.cyan( dest_folder ) );
+    }
+
+    gulp.src( src_globs )
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe( gulp.dest( dest_folder ).on('finish', jadeComplete) );
+}
+
+module.exports = HTML;

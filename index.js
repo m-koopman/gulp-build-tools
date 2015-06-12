f2cddb03ea3c4a786b09755f74c731ff6c71f8e6
Max Koopman <m_koopman@me.com>
2015-06-12 15:20:25 +1000

    Added image and template modules to default namespace

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ module.exports = {
     'livereload': require("./livereload.js"),
     'scripts': require("./scripts.js"),
     'static': require("./static.js"),
-    'style': require("./style.js")
+    'style': require("./style.js"),
+    'images': require("./images.js"),
+    'template': require("./template.js")
 }
